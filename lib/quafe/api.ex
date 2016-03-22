b1ad16de04706f7694436b9e96d2e9f46dc0dc78
Harry Reeder <harry@harryreeder.co.uk>
2016-03-22 13:57:14 +0000

    Wrapper Functions
    Provide wrapped functions for authenticated calls

--- lib/quafe/api.ex
+++ lib/quafe/api.ex
@@ -1,8 +1,22 @@
 defmodule Quafe.API do
   @user_agent [{"User-agent", "Quafe 0.1 (https://github.com/hreeder/quafe)"}]
-  
+
+  def get_authenticated(api_key, endpoint, args \\ %{}) do
+    api_key
+    |> fetch_from_CCP_with_key(endpoint, args)
+    |> process_response
+  end
+
+  def get(api_key, endpoint, args \\ %{}) do
+    endpoint
+    |> fetch_from_CCP(args)
+    |> process_response
+  end
+
   def fetch_from_CCP_with_key(api_key, endpoint, args \\ %{}) do
-    
+    endpoint
+    |> fetch_from_CCP(Dict.merge(args, %{ keyID: api_key.keyid,
+                                          vCode: api_key.vcode}))
   end
 
   def fetch_from_CCP(endpoint, args \\ %{}) do
